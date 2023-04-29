6514d76c798c9f77d40dc2f1f3d38463c2424aa2
Christine Morrill <christinenicolemorrill@gmail.com>
2023-04-29 13:56:14 -0400

    Initial Commit

---
+++ Question App/Question_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_AppApp.swift
+//  Question App
+//
+//  Created by Christine Pitino on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Question_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
